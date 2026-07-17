25eb55fe2902d2d434afe02f8a09eb946fcb514c
michaelBullfrog <165947213+michaelBullfrog@users.noreply.github.com>
2026-07-17 13:45:31 -0400

    Update main.py

--- bullfrog-intelligence-mvp/backend/app/main.py
+++ bullfrog-intelligence-mvp/backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://YOUR-FRONTEND-URL.onrender.com",],
+    allow_origins=["http://localhost:3000", "https://bullfrog-intelligence-frontend.onrender.com",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
